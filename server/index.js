9aec89cc29d63c70c516b6036fef77d90c453235
Dave Bennett <david.bennett@rws.com>
2026-01-03 18:26:41 +0000

    Fix express error

--- server/index.js
+++ server/index.js
@@ -52,7 +52,7 @@ app.post('/api/data', async (req, res) => {
 // Serve static files in production
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../dist')));
-    app.get('*', (req, res) => {
+    app.get('*path', (req, res) => {
         res.sendFile(path.join(__dirname, '../dist/index.html'));
     });
 }
